63d30b0a4ccf991aee870c380d72927b5fc80fc5
corey clarke <sombas100@yahoo.com>
2024-06-17 23:46:51 +0100

    customise sidebar

--- client/src/components/CustomSidebar.tsx
+++ client/src/components/CustomSidebar.tsx
@@ -5,6 +5,7 @@ import styles from "./CustomSidebar.module.css";
 import { FaUserGroup } from "react-icons/fa6";
 import { GiReceiveMoney } from "react-icons/gi";
 import { TbLogout } from "react-icons/tb";
+import { MdOutlineWork } from "react-icons/md";
 
 export const CustomSidebar = () => {
   return (
@@ -14,21 +15,27 @@ export const CustomSidebar = () => {
         <ul>
           <li>
             <Link to="/staff" className={styles.sidebarItem}>
-              <FaUserGroup style={{ marginRight: "10px" }} />
+              <FaUserGroup size={25} style={{ marginRight: "10px" }} />
               Staff
             </Link>
           </li>
           <li>
             <Link to="/salary" className={styles.sidebarItem}>
-              <GiReceiveMoney style={{ marginRight: "10px" }} />
+              <GiReceiveMoney size={25} style={{ marginRight: "10px" }} />
               Salary
             </Link>
           </li>
           <li>
-            <Link to="/login" className={styles.sidebarItem}>
-              <TbLogout style={{ marginRight: "10px" }} />
-              Logout
+            <Link to="/attendance" className={styles.sidebarItem}>
+              <MdOutlineWork size={30} style={{ marginRight: "10px" }} />
+              Attendance
             </Link>
+            <li>
+              <Link to="/login" className={styles.sidebarItem}>
+                <TbLogout size={25} style={{ marginRight: "10px" }} />
+                Logout
+              </Link>
+            </li>
           </li>
         </ul>
       </div>
