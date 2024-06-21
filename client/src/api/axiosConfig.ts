bc34c32a90d6ec2725458aca9ce928ba4e3f476a
corey clarke <sombas100@yahoo.com>
2024-06-21 15:07:27 +0100

    changes

--- client/src/api/axiosConfig.ts
+++ client/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://staffify.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
